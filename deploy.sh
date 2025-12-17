988db906dfde789f5710a8960c36baa6f1857294
Mark Genesis Romantigue <markg.romantigue@gmail.com>
2025-12-17 23:25:04 +0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -45,6 +45,9 @@ info "Installing npm dependencies"
 # Optimized npm install (forcing devDependencies for build)
 npm install --include=dev --no-audit --no-fund --quiet
 
+info "Fixing permissions on local binaries"
+chmod +x node_modules/.bin/* || true
+
 info "Building assets (SCSS, Tailwind, JS)"
 npm run build
 
